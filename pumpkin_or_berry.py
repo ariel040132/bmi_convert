93d5da5583ef71ba643966bebef400bc57fe1ba9
Ariel Guo <ariel040132@gmail.com>
2022-08-15 14:19:09 +0800

    berry chosen

---
+++ pumpkin_or_berry.py
@@ -0,0 +1,17 @@
+m = input("small or not？:")
+
+if m == "small":
+        n = input("the color of fruit is green or red?:")
+        if n == "green":
+                print("It's a pea.")
+        else:
+                print("It's a berry.")
+else:
+        x = input("the color of fruit is green or orange?:")
+        if x == "green":
+                print("It's a watermelon!")
+        else:
+                print("It's a pumpkin!")
+            
+
+
