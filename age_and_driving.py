14ea88b2b8e947f8e383bb1f54acdaad2b068c45
Ariel Guo <ariel040132@gmail.com>
2022-08-15 16:33:26 +0800

    修改版

--- age_and_driving.py
+++ age_and_driving.py
@@ -1,4 +1,7 @@
 driving = input("請問你有沒有開過車？：")
+if driving != "有" and driving != "沒有":
+	print("請輸入 有／沒有")
+	raise SystemExit
 age = input("請問你的年齡？：")
 age = int(age)
 if driving == "有":
@@ -6,7 +9,7 @@ if driving == "有":
 		print("恭喜通過測驗")
 	else:
 		print("奇怪 你怎麼會有開過車？")
-else:
+elif driving == "沒有":
 	if age >= 18:
 		print("你可以去考駕照")
 	else:
